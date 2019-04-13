5ef2a90c8582e5cff49538a8e2debde6859df30f
Douglas Uretsky <douglasuretsky@gmail.com>
2019-04-12 20:37:27 -0400

    create routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :account_number, uniqueness: true
   has_one :account
   has_one :balance, through: :account
+  validates :account_number, presence: true
+  # before_save :encrypted_account_number
+
+  # private
+  # def encrypted_account_number
+  #    # salt = ENV['SALT'] # We save the value of: SecureRandom.random_bytes(64)
+  #    key = ENV['KEY']   # We save the value of: ActiveSupport::KeyGenerator.new('password').generate_key(salt)
+  #    crypt = ActiveSupport::MessageEncryptor.new(key)
+  #    consumer_key = self.account_number # Input value from our form
+  #    encrypted_data = crypt.encrypt_and_sign(consumer_key) # or crypt.encrypt_and_sign(self.consumer_key)
+  #    self.consumer_key = encrypted_data
+  #    # You can refactor to make these steps shorter.
+  #  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
-  resources :users
 
-  resources :accounts do
+  resources :users do
+    resources :accounts
+  end
+
+  resources :users do
     resources :balances
   end
 end
